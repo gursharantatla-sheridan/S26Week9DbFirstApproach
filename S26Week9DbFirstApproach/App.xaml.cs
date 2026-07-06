f8a32fbc51324392cc39f2c7dd7c6c37116bba39
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-07-06 16:40:08 -0400

    Add project files.

---
+++ S26Week9DbFirstApproach/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace S26Week9DbFirstApproach
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
